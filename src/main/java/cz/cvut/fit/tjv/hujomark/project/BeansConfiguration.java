21f9784f56ea0bcfae95594b46c71ed9b6de171d
Marko Hujo <hujomark@fit.cvut.cz>
2021-10-29 16:05:44 +0200

    Setup db

---
+++ src/main/java/cz/cvut/fit/tjv/hujomark/project/BeansConfiguration.java
@@ -0,0 +1,9 @@
+package cz.cvut.fit.tjv.hujomark.project;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class BeansConfiguration {
+}
