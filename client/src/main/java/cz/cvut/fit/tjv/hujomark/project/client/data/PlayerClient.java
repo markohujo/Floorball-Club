0c47e6c1832d7c669430e7905afa4a6565de0513
Marko Hujo <hujomark@fit.cvut.cz>
2021-12-23 10:06:03 +0100

    Minor changes in FE

--- client/src/main/java/cz/cvut/fit/tjv/hujomark/project/client/data/PlayerClient.java
+++ client/src/main/java/cz/cvut/fit/tjv/hujomark/project/client/data/PlayerClient.java
@@ -1,6 +1,7 @@
 package cz.cvut.fit.tjv.hujomark.project.client.data;
 
 import cz.cvut.fit.tjv.hujomark.project.client.model.PlayerDto;
+import cz.cvut.fit.tjv.hujomark.project.client.model.TeamDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -18,5 +19,20 @@ public class PlayerClient {
         return playerWebClient.get()
                 .retrieve()
                 .bodyToFlux(PlayerDto.class);
+    }
+
+    public Flux<TeamDto> teams(Long id) {
+        return playerWebClient.get()
+                .uri("/{id}/teams", id)
+                .retrieve()
+                .bodyToFlux(TeamDto.class);
+    }
+
+    public void delete(Long id) {
+        playerWebClient.delete()
+                .uri("/{id}", id)
+                .retrieve()
+                .toBodilessEntity()
+                .subscribe();
     }
 }
--- client/src/main/java/cz/cvut/fit/tjv/hujomark/project/client/ui/PlayerWebController.java
+++ client/src/main/java/cz/cvut/fit/tjv/hujomark/project/client/ui/PlayerWebController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/players")
@@ -20,5 +21,22 @@ public class PlayerWebController {
     public String list(Model model) {
         model.addAttribute("players", playerClient.readAll());
         return "players";
+    }
+
+    @GetMapping("/teams")
+    public String teams(@RequestParam Long id, Model model) {
+        model.addAttribute("teams", playerClient.teams(id));
+        return "teams";
+    }
+
+    @GetMapping("/matches")
+    public String matches(@RequestParam Long id, Model model) {
+        return "matches";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam Long id, Model model) {
+        playerClient.delete(id);
+        return "redirect:/players";
     }
 }
