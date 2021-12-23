8aa23b69ef737082594b50a8819cf0eee2509ed8
Marko Hujo <hujomark@fit.cvut.cz>
2021-12-23 09:11:08 +0100

    Added list all players functionality in frontend

--- client/src/main/java/cz/cvut/fit/tjv/hujomark/project/client/data/PlayerClient.java
+++ client/src/main/java/cz/cvut/fit/tjv/hujomark/project/client/data/PlayerClient.java
@@ -1,4 +1,22 @@
 package cz.cvut.fit.tjv.hujomark.project.client.data;
 
+import cz.cvut.fit.tjv.hujomark.project.client.model.PlayerDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+
+@Component
 public class PlayerClient {
+    private final WebClient playerWebClient;
+
+    public PlayerClient(@Value("${backend_url}") String backendUrl) {
+        this.playerWebClient = WebClient.create(backendUrl + "/players");
+    }
+
+    public Flux<PlayerDto> readAll() {
+        return playerWebClient.get()
+                .retrieve()
+                .bodyToFlux(PlayerDto.class);
+    }
 }
--- client/src/main/java/cz/cvut/fit/tjv/hujomark/project/client/ui/PlayerWebController.java
+++ client/src/main/java/cz/cvut/fit/tjv/hujomark/project/client/ui/PlayerWebController.java
@@ -1,4 +1,24 @@
 package cz.cvut.fit.tjv.hujomark.project.client.ui;
 
+import cz.cvut.fit.tjv.hujomark.project.client.data.PlayerClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/players")
 public class PlayerWebController {
+    private final PlayerClient playerClient;
+
+
+    public PlayerWebController(PlayerClient playerClient) {
+        this.playerClient = playerClient;
+    }
+
+    @GetMapping
+    public String list(Model model) {
+        model.addAttribute("players", playerClient.readAll());
+        return "players";
+    }
 }
